4d147af676d3919a4773a3c32218b7605597e4ee
rmatheney7 <rmatheney7@gmail.com>
2017-10-10 12:36:30 -0500

    [Feat] Added gps

--- App/Component/Styles/MapViewStyle.js
+++ App/Component/Styles/MapViewStyle.js
@@ -4,8 +4,7 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     container: {
         ...StyleSheet.absoluteFillObject,
-        height: 400,
-        width: 400,
+        
         justifyContent: 'flex-end',
         alignItems: 'center',
       },
