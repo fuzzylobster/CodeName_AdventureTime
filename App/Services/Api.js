b73f338c1d6d7fa8715108ecf0718998277ecf09
Violet Suber <violetmands@gmail.com>
2017-10-18 15:51:04 -0500

    (merge)

--- App/Services/Api.js
+++ App/Services/Api.js
@@ -1,28 +1,14 @@
-<<<<<<< 5f5563a2095d92bfe26c0efde945116be682b4ce
 import apisauce from "apisauce";
 const create = (baseURL = "http://e502038f.ngrok.io") => {
-=======
-import apisauce from 'apisauce'
-const create = (baseURL = 'http://192.168.1.209:3030') => {
->>>>>>> (merge)
   const api = apisauce.create({
     baseURL,
     headers: {
       "Cache-Control": "no-cache"
     },
     timeout: 10000
-<<<<<<< 5f5563a2095d92bfe26c0efde945116be682b4ce
   });
 
   const postUserData = user => api.post("/users", user);
-=======
-  })
-  
-  const postUserData = (user) => {
-    // debugger;
-    return api.post('/authentication', user);
-  }
->>>>>>> (merge)
   return {
     postUserData
   };
